bc1c8bf6dcfd845757ca18d8dc4670158c67b793
MikeChigev <mchigev@gmail.com>
2022-11-21 14:31:28 +0300

    ex5

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Oh this is Masha");
+}
+else
+{
+    Console.Write("Hellow,  ");
+    Console.WriteLine(username);
+}
